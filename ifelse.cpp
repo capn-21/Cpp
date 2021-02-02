64b607c75945911580b6ddac8789fa4b326c6eb2
Omkar Chalke <65553502+capn-21@users.noreply.github.com>
2021-02-02 22:42:15 +0530

    initial commit

---
+++ ifelse.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	bool male= false;
+	bool fat=false;
+	
+	if(!male && !fat){
+	cout<<" fat male";}
+	
+	else if(male){
+		cout<<"slim malr";
+	}
+			
+	else{
+	cout<<	"female";
+	}
+}
